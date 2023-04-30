901b5d42b57e3df958fb89f217da074457f7f4c7
Alan Reed Wagner <wagne280@msu.edu>
2023-04-30 13:22:12 -0400

    Added Deposit and Withdraw

--- Account.py
+++ Account.py
@@ -1,4 +1,29 @@
 class Account:
 
-    def __init__(self):
-        pass
+    owner = ""
+    type = ""
+    checkings = 0
+    savings = 0
+
+    def __init__(self, owner, accountType):
+        self.owner = owner
+        self.type = accountType
+        self.balance = 0
+
+    def deposit(self, value, account):
+        if(value >= 0):
+            if(account == 'c'):
+                self.balance += value
+            if(account == 's'):
+                self.savings += value
+            return True
+        return False
+
+    def withdraw(self, value):
+        if(value <= self.balance):
+            self.balance -= value
+            return True
+        return False
+    
+
+        
