a8bcae6402ddd40afa813cb85b40036b0ad06537
Alan Reed Wagner <wagne280@msu.edu>
2023-04-30 13:57:38 -0400

    Added Create, Delete, and Display

--- Bank.py
+++ Bank.py
@@ -2,5 +2,23 @@ import Account
 
 class Bank:
 
-    def __init__(self):
-        pass
+    accounts = []
+
+    def __init__(self, accounts):
+        self.accounts = accounts
+
+    def createAccount(self):
+        owner = input("Enter Account Name")
+        accountType = input("Enter Account Type")
+        account = Account(owner, accountType)
+        self.accounts.append(account)
+
+    def deleteAccount(self, accountNum):
+        for account in self.accounts:
+            if(account.getNum() == accountNum):
+                self.accounts.remove(account)
+
+    def display(self):
+        for account in self.accounts:
+            account.display()
+            print("")
