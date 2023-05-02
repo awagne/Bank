879629e485e1dc1ec41724f78bb381bfe75ef78c
Alan Reed Wagner <wagne280@msu.edu>
2023-05-02 17:35:22 -0400

    Added GetAccount

--- Bank.py
+++ Bank.py
@@ -8,15 +8,22 @@ class Bank:
         self.accounts = accounts
 
     def createAccount(self):
-        owner = input("Enter Account Name")
-        accountType = input("Enter Account Type")
-        account = Account(owner, accountType)
+        owner = input("Enter Account Name: ")
+        account = Account.Account(owner)
         self.accounts.append(account)
+        account.display()
 
     def deleteAccount(self, accountNum):
         for account in self.accounts:
             if(account.getNum() == accountNum):
                 self.accounts.remove(account)
+
+    def getAccount(self,name, accountNum):
+        for account in self.accounts:
+            if(account.getOwner == name and account.getNum() == accountNum):
+                return account
+            
+        return None
 
     def display(self):
         for account in self.accounts:
