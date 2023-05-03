49ce8a1aacb4f4d633974d604c99a3ad3379e6fc
Alan Reed Wagner <wagne280@msu.edu>
2023-05-02 20:03:14 -0400

    Created user input

--- main.py
+++ main.py
@@ -14,9 +14,27 @@ if __name__ == "__main__":
             num = input("Enter Account Number: ")
             bank.deleteAccount(num)
         elif choice == "C":
-            name = print("Enter Account Holder: ")
+            name = input("Enter Account Holder: ")
             num = input("Enter Account Number: ")
             account = bank.getAccount(name, num)
+            account.display()
 
-        print("What would you like to do?")
+            print("\nWhat would you like to do?")
+            choice = input("A) Deposit\nB) Withdraw\nC) Quit\n")
+            while choice != "C":
+                if choice == "A":
+                    print("Which account would you like to deposit in?")
+                    accountType = input("Enter c for Checking\n Enter s for Savings\n")
+                    value = input("How much would you like to deposit?")
+                    if not account.deposit(value, accountType):
+                        print("Invalid Deposit")
+
+                elif choice == "B":
+                    print("Which account would you like to withdraw from?")
+                    accountType = input("Enter c for Checking\n Enter s for Savings\n")
+                    value = input("How much would you like to withdraw?")
+                    if not account.withdraw(value, accountType):
+                        print("Invalid Withdrawl")
+            
+        print("\nWhat would you like to do?")
         choice = input("A) Create Account\nB) Delete Account\nC) Sign in\nD) Quit\n")
